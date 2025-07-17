587d106109553e0693e672ca8d1d5390986a65be
Ayla-Abdullahi <166896538+Ayla-Abdullahi@users.noreply.github.com>
2025-07-17 10:41:50 +0300

    additional files added

---
+++ python.plp/methods.py
@@ -0,0 +1,9 @@
+name = "Ayla"
+print(len(name))  # Output: 4
+print(name.capitalize())  # Output: Ayla
+print(name.upper())  # Output: AYLA
+print(name.lower())  # Output: ayla
+print(name.find("A"))  # Output: 0
+print(name.replace("A", "E"))  # Output: Eyla
+print(name.isdigit())  # Output: False
+print(name.isalpha())  # Output: True
---
+++ python.plp/type-casting.py
@@ -0,0 +1,11 @@
+#type casting is converting the data type of a variable to another type
+y=1
+x="2"
+z=3.0
+#so in order to type cast you just indicatte the type you want to set to and pull the variable to be type casted inside a brackets next to the type
+x=int(x) 
+y=float(y)
+z=str(z)
+print(y)
+print(x*3)
+print(z*3 )  # This will repeat the string '3.0' three times
